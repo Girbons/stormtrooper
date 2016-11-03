ccbb0c0fd2c6c7ac432442065ea650455d81dbdb
Federico Frenguelli <synasius@gmail.com>
2016-11-03 17:04:27 +0100

    Fixed a couple of issues in dockerfile and development settings

--- {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings/dev.py
+++ {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings/dev.py
@@ -7,9 +7,9 @@ DEBUG = True
 
 SECRET_KEY = env('DJANGO_SECRET_KEY', '1234567890')
 
-INTERNAL_IPS = (
-    '127.0.0.1',
-)
+INTERNAL_IPS = ['127.0.0.1']
+
+ALLOWED_HOSTS = []
 
 
 # Enabling console loggers
